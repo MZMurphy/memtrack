05621a2d93169ceaa9bcd935ef44ff423c247521
zaraOnDell <mzaramurphy@gmail.com>
2025-03-15 10:50:52 +0000

    dlsym of original dynamic memory functions and function pointers

--- src/originalDynamicMemoryFunctions.c
+++ src/originalDynamicMemoryFunctions.c
@@ -1,15 +1,66 @@
 
+#define _GNU_SOURCE // For dlsym with RTLD_NEXT
+//#define RTLD_NEXT (void *)-1
 #include <stdio.h>
-// within constructor link to original dynamic memory functions using dlsym...
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <unistd.h>
 
 
+// function pointers
+void*(*original_malloc)(size_t size);
 
-//original_free(void)(void* ptr) = dlsym("free")... ;  
+void*(*original_calloc)(size_t num, size_t size);
 
-//original_malloc(void*)(size_t size) = dlysm("malloc")... ;
+void*(*original_realloc)(void* ptr, size_t size);
+
+void(*original_free)(void* ptr);
+
+// within constructor link to original dynamic memory functions using dlsym... 
+int main(void)
+{
+    original_malloc = dlsym(RTLD_NEXT, "malloc");
+    if(!original_malloc)
+    {
+        write(2, "Error: Could not find original malloc.\n", 40);
+        exit(1);
+    }
+
+    
+    original_calloc = dlsym(RTLD_NEXT, "calloc");
+    if(!original_calloc)
+    {
+        write(2, "Error: Could not find original calloc.\n", 40);
+        exit(1);
+
+    }
+
+    original_realloc = dlsym(RTLD_NEXT, "realloc");
+    if(!original_realloc)
+    {
+        write(2, "Error: Could not find original realloc.\n", 41);
+        exit(1);
+    }
+
+    original_free = dlsym(RTLD_NEXT, "free");
+    if(!original_free)
+    {
+        write(2, "Error: Could not find original free.\n", 48);
+        exit(1);
+    }
 
 
-//original_calloc(void*)(int n_items, size_t size) = dlysm("calloc")... ;
+    int * ptrInt = original_malloc(10);
+    if(ptrInt == NULL)
+    {
+        printf("NULL ptr returned");
+    }else{
+        printf("Memory allocated at address %p\n", ptrInt);
+        free(ptrInt);
+    }
 
+    printf("\nEnd\n");
+    return 0;
 
-//original_realloc(void*)(void* oldPtr, size_t size) = dlysm("realloc") ... ;
+}
+
